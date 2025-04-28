37cfa9ef869c8970887a7906b1462323142599bd
ricardocotillo <ricardo.cotillo@gmail.com>
2025-04-27 20:44:26 -0500

    Add EmailFile and HtmlPage controllers to Email module

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailParserController } from './email-parser.controller';
 import { EmailParserService } from './email-parser.service';
+import { EmailFileController } from './email-file.controller';
+import { HtmlPageController } from './html-page.controller';
 
 @Module({
-  controllers: [EmailParserController],
+  controllers: [EmailParserController, EmailFileController, HtmlPageController],
   providers: [EmailParserService],
   exports: [EmailParserService],
 })
